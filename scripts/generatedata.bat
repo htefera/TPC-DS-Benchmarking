dcb9797f014a56a290d1ce7039977fd440881717
Haftamu Hailu <32423747+htefera@users.noreply.github.com>
2022-01-27 11:59:36 +0100

    Update generatedata.bat

--- scripts/generatedata.bat
+++ scripts/generatedata.bat
@@ -5,6 +5,6 @@ REM Replace SCALE_FACTOR and local directories variables with custom ones
 
 SET SCALE_FACTOR = 1
 
-SET OUTPUT_DIRECTORY = C:\Users\karim\Desktop\data
+SET OUTPUT_DIRECTORY =  C:\Users\haftish\Desktop\data
 
 dsdgen /scale !SCALE_FACTOR! /dir !OUTPUT_DIRECTORY!  /terminate n
